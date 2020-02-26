1a33d244a38870d8a9900639832d42aae6a9e986
David Ondrušek <ondrudav@seznam.cz>
2020-02-26 18:59:42 +0100

    working vga

--- vhdl_learning/vhdl_learning.srcs/sources_1/new/main.vhd
+++ vhdl_learning/vhdl_learning.srcs/sources_1/new/main.vhd
@@ -6,41 +6,42 @@ entity main is
     generic (
 --Units: pixels * 2 (50 MHz pixel clock on 100 MHz system)
         hvis :integer:= 800*2;
+--        hvis :integer:= 50*2;
         hfporch :integer:= 56*2;
         hsync :integer:= 120*2;
         hbporch :integer:= 64*2;
 --Units: lines
         vvis :integer:= 600;
+--        vvis :integer:= 50;
         vfporch :integer:= 37;
         vsync :integer:= 6;
         vbporch :integer:= 23);
     port(
         clk : in std_logic;
-        r, g, b : out std_logic_vector(0 to 3);
-        hsyncout : out std_logic;
-        vsyncout : out std_logic
+        r, g, b : out std_logic_vector(3 downto 0);
+        hsyncout, vsyncout: out std_logic := '0'
     );
 end main;
 
 architecture synth of main is
-    signal x, y: unsigned(0 to 15) := (others => '0');
+    signal x, y, frame: unsigned(15 downto 0) := (others => '0');
 begin
     pixel_output :process(clk)
-        variable tx : unsigned := x + 1;
-        variable ty : unsigned := y + 1;
+        variable tx, ty : unsigned(15 downto 0);
     begin if rising_edge(clk) then
+        tx := x + 1;
+        ty := y + 1;
         x <= tx;
         r <= (others => '0');
         g <= (others => '0');
         b <= (others => '0');
         hsyncout <= '0';
-        vsyncout <= '0';
-            
+                    
         if tx < hvis then
             if y < vvis then
-                r <= std_logic_vector(x(1 to 4));
-                g <= std_logic_vector(y(0 to 3));
-                b <= std_logic_vector(y(2 to 5));
+                r <= std_logic_vector(x(6 downto 3));
+                g <= std_logic_vector(y(3 downto 0));
+                b <= std_logic_vector(frame(6 downto 3));
             end if;
         elsif tx < hvis + hfporch then
         elsif tx < hvis + hfporch + hsync then
@@ -54,11 +55,31 @@ begin
             elsif ty < vvis + vfporch then
             elsif ty < vvis + vfporch + vsync then
                 vsyncout <= '1';
-            elsif ty < vvis + vfporch + vsync then
+            elsif ty < vvis + vfporch + vsync + vbporch then
+                vsyncout <= '0';
             else
                 y <= (others => '0');
+                frame <= frame + 1;
             end if;
         end if;
     end if;
     end process;
 end synth;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity sim is
+end sim;
+
+architecture sim of sim is
+signal clk : std_logic := '1';
+begin
+    dut : entity work.main(synth) port map (clk => clk);
+    process begin
+    clk <= not clk;
+    wait for 5 ns;
+    end process;
+end sim;
